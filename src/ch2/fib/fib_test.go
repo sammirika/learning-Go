139e671b4db89cdbc93a337c8a8c4bb1e5ed9a45
sammirika <1755994546@qq.com>
2021-12-20 15:11:49 +0800

    2021.12.20:go语言变量赋值

---
+++ src/ch2/fib/fib_test.go
@@ -0,0 +1,39 @@
+package fib
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFibList(t *testing.T) {
+	//var a int = 1
+	//var b int = 1
+	//var (
+	//	a int = 1
+	//	b int = 1
+	//)
+	a := 1
+	b := 1
+	fmt.Println(a)
+	for i := 0; i < 5; i++ {
+		fmt.Println(" ", b)
+		tmp := a
+		a = b
+		b = tmp + a
+	}
+	fmt.Println()
+}
+
+func TestExchange(t *testing.T) {
+	a := 1
+	b := 12
+	//fmt.Println("a: ",a)
+	//fmt.Println("b: ",b)
+	//temp:=a
+	//a=b
+	//b=temp
+	//fmt.Println("a: ",a)
+	//fmt.Println("b: ",b)
+	a, b = b, a
+	t.Log(a, b)
+}
